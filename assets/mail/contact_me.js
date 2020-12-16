882358e23ef18a937010f0e99ec6710c64975a3f
f964821 <73145119+f964821@users.noreply.github.com>
2020-12-16 17:47:05 -0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -55,9 +55,9 @@ $(function () {
                         .append("</button>");
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
-                            "Sorry " +
+                            "Disculpa " +
                                 firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                                ", parece que el servidor no responde. Por favor escribinos por Instagram o Facebook!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
